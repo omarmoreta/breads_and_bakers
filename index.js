a48ca41b0d09a0702bd486e14e652182e94eebc6
Omar <o.moreta@gmail.com>
2023-10-13 14:47:56 -0400

    added middleware for views and jsx directories

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const breadsController = require("./controllers/breads");
+const createEngine = require("express-react-views").createEngine();
+
+// Middleware
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", createEngine);
 
 // GET /
 app.get("/", (req, res) => {
