bec242361e05e99f652db8cfe5486de7c7acbacb
Omar <o.moreta@gmail.com>
2023-10-13 13:45:49 -0400

    added breads model and imported data into GET /breads index route

--- controllers/breads.js
+++ controllers/breads.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const breads = express.Router();
+const Bread = require("../models/bread");
 
 // GET /breads
 breads.get("/", (req, res) => {
-  res.send("This is the index at /breads");
+  res.send(Bread);
 });
 
 module.exports = breads;
