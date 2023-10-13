ee4a8016e6d1b8853d0f38340f49c22e3786cf7e
Omar <o.moreta@gmail.com>
2023-10-13 15:40:36 -0400

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -5,7 +5,20 @@ function Index ({ breads }) {
     return (
         <Default>
             <h2>Index Page</h2>
-            <p>I have {breads[0].name} bread!</p>
+            {/* <p>I have {breads[0].name} bread!</p>*/}
+            <ul>
+                {
+                    breads.map( ( bread, index) => {
+                        return (
+                            <li key={ index }>
+                                <a href={`/breads/${ index }`}>
+                                    { bread.name }
+                                </a>                                
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </Default>
     )
 };
